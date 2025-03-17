bf517a21df30c0096a1a49beee0a6edf16b681a3
Dani <danielml2003.dml@gmail.com>
2025-03-17 21:22:00 +0000

    wip: Loader. Added in Exercices Json load data

--- Templates/JS/Loader.js
+++ Templates/JS/Loader.js
@@ -45,3 +45,41 @@ const loadDescription = () =>{
             document.getElementsByClassName('main-description')[0].textContent = data.description;
         })
 }
+function loadUserExercises(){
+    fetch("../JsonFiles/exercises.json")
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Failed to load users exercises");
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(element => {
+                addTemplate("grid", "exerciseRecipeCard.html", element);
+            })
+        })
+}
+function addTemplate(id, url, item) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Fail loading ${url}`);
+            }
+            return response.text();
+        })
+        .then(html => {
+            const container = document.getElementById(id);
+            const newElement = document.createElement("div");
+            newElement.innerHTML = html;
+
+            const label = newElement.querySelector(".card-label");
+            const title = newElement.querySelector(".card-title");
+
+            label.textContent = item.type;
+            title.textContent = item.name;
+
+            container.appendChild(newElement);
+        })
+        .catch(error => console.error(error));
+}
+
